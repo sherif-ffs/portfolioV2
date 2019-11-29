7160ee32935cb644797533f6a6a4826c07ae8291
Sherif Elmetwally <sge5@njit.edu>
2019-11-29 00:49:39 -0500

    build skeleton of homepage

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,8 +1,4 @@
-const toggleMenu = document.querySelector(".navigation button");
-const menu = document.querySelector(".navigation ul");;
-
-toggleMenu.addEventListener("click", function () {
-  const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
-  toggleMenu.setAttribute("aria-expanded", !open);
-  menu.hidden = !menu.hidden;
-});
+  
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+  document.getElementById('nav').classList.toggle('active');
+})
