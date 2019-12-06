6638c3768c857d3f34cf2cc6189b2a9e4121983d
Sherif Elmetwally <sge5@njit.edu>
2019-12-05 19:49:05 -0500

    tweak navbar among other things

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,4 +1,19 @@
   
 document.querySelector('.menu-toggle').addEventListener('click', function() {
   document.getElementById('nav').classList.toggle('active');
-})
+});
+
+document.querySelector('.nav-title').addEventListener('mouseenter', () => {
+  document.querySelector('.left-bracket').style.paddingRight = "8px";
+  document.querySelector('.left-bracket').style.color = "#D34759";
+  document.querySelector('.right-bracket').style.paddingLeft = "8px";
+  document.querySelector('.right-bracket').style.color = "#D34759";
+
+});
+
+document.querySelector('.nav-title').addEventListener('mouseleave', () => {
+  document.querySelector('.left-bracket').style.paddingRight = "0px";
+  document.querySelector('.left-bracket').style.color = "#323648";
+  document.querySelector('.right-bracket').style.paddingLeft = "0px";
+  document.querySelector('.right-bracket').style.color = "#323648";
+});
